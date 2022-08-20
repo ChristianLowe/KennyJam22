f92bfcca6131fa372a8ba9bd620dca82257bd292
Christian Lowe <christianlowe.cl@gmail.com>
2022-08-19 21:41:25 -0700

    Add Pet

---
+++ Scripts/Pet.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+var drive = {
+	"hunger": 100.0,
+	"thirst": 100.0,
+	"sleepy": 100.0,
+	"boring": 100.0,
+}
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta):
+	pass
