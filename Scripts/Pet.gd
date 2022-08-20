220e8845b395b0f2d182ec200507c8032198b495
David Dalisay <dalisay.david3@gmail.com>
2022-08-20 10:26:53 -0400

    Pet can now shake uncontrollably, sway back and forth, and have a hungry face.

--- Scripts/Pet.gd
+++ Scripts/Pet.gd
@@ -7,12 +7,59 @@ var drive = {
 	"boring": 100.0,
 }
 
+export var sway_amount = 20.0
+export var shake_amount = 5.0
+
+var sway_direction_amount = [-sway_amount, sway_amount]
+
 func _ready():
 	$SquishTween.connect("tween_all_completed", self, "unsquish")
 	$UnsquishTween.connect("tween_all_completed", self, "squish")
+	$SwayTween.connect("tween_completed", self, "_on_sway_tween_completed")
 	
-	squish()
+	sway()
 
+func sway() -> void:
+	$SwayTween.interpolate_property(
+		$Sprite,
+		"rotation_degrees",
+		sway_direction_amount[0],
+		sway_direction_amount[1],
+		1.0,
+		Tween.TRANS_BACK,
+		Tween.EASE_IN_OUT,
+		0.05
+	)
+	$SwayTween.start()
+	
+func _on_sway_tween_completed(object, key) -> void:
+	# Sway back the other way.
+	sway_direction_amount.invert()
+	sway()
+
+func stop_sway() -> void:
+	$SwayTween.stop_all()
+	$SwayTween.reset_all()
+	$SwayTween.remove_all()
+	
+	$Sprite.rotation_degrees = 0.0
+	sway_amount = [-20.0, 20.0]
+	
+func shake(iterations: int = 5) -> void:
+	for i in range(iterations):
+		$ShakeTween.interpolate_property(
+			$Sprite,
+			"offset:x" if randf() < 0.5 else "offset:y",
+			0.0,
+			shake_amount,
+			0.1,
+			Tween.TRANS_BOUNCE,
+			Tween.EASE_OUT
+		)
+		
+		$ShakeTween.start()
+		yield($ShakeTween,"tween_all_completed")
+	
 func squish() -> void:
 	var normal_scale = $Sprite.scale
 	$SquishTween.interpolate_property(
@@ -58,6 +105,13 @@ func unsquish() -> void:
 	)
 	$UnsquishTween.start()
 
+func _input(event):
+	# This event handler is just for testing using the SPACE BAR.
+	if event.is_action_pressed("ui_accept"):
+		if $SwayTween.is_active():
+			stop_sway()
+		else:
+			sway()
 
 func _process(delta):
 	pass
