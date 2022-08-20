84bea872a2333f93f6370eda393b576e2ba57769
Christian Lowe <christianlowe.cl@gmail.com>
2022-08-20 12:04:39 -0700

    Clicking egg give yamo

--- Scripts/Egg.gd
+++ Scripts/Egg.gd
@@ -1,15 +1,17 @@
 extends Area2D
 
-var health = 5
+var health = 15
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 	
-
 func _input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.is_pressed():
 		bounce()
+		health -= 1
+		if health <= 0:
+			pop()
 
 func bounce():
 	var small = Vector2(0.7, 0.7)
@@ -21,6 +23,14 @@ func bounce():
 		Tween.TRANS_BOUNCE, Tween.EASE_OUT
 	)
 	$Tween.start()
+
+func pop():
+	var pet_scene = load("res://Actors/Pet.tscn")
+	var pet = pet_scene.instance()
+	pet.position = position
+	get_parent().add_child(pet)
+	queue_free()
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
