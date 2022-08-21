c352f7cae2122320b969f8c3ab146a6b742a20e8
David Dalisay <dalisay.david3@gmail.com>
2022-08-20 23:28:23 -0400

    Added grow/evolutions based on time.

--- Scripts/Pet.gd
+++ Scripts/Pet.gd
@@ -6,25 +6,44 @@ enum Status {
 	Content,
 	Bored,
 	Hungry,
-	NeedsAttention,
-	Sleepy
+	Thirsty,
+	NeedsLove,
+	Sleepy,
+	Trashy
 }
+
+var sway_amount = 10.0
+var shake_amount = 5.0
+
+export var hunger_drive_delta: float = 0.010
+export var thirst_drive_delta: float = 0.015
+export var sleepy_drive_delta: float = 0.033
+export var boring_drive_delta: float = 0.055
+export var loving_drive_delta: float = 0.095
+export var trashy_drive_delta: float = 0.025
+
+export var food_reward_amount: float = 15.0
+export var drink_reward_amount: float = 15.0
+export var pet_reward_amount: float = 15.0
+export var love_reward_amount: float = 15.0
+export var clean_reward_amount: float = 15.0
+
+export var max_seconds_until_evolve: int = 60.0*10.0
+onready var seconds_until_evolve: int = max_seconds_until_evolve
 
 var drive = {
-	"hunger": [100.0, 0.010],
-	"thirst": [100.0, 0.015],
-	"sleepy": [100.0, 0.033],
-	"boring": [100.0, 0.055],
-	"loving": [100.0, 0.095],
-	"trashy": [100.0, 0.025]
+	"hunger": [100.0, hunger_drive_delta, Status.Hungry],
+	"thirst": [100.0, thirst_drive_delta, Status.Thirsty],
+	"sleepy": [100.0, sleepy_drive_delta, Status.Sleepy],
+	"boring": [100.0, boring_drive_delta, Status.Bored],
+	"loving": [100.0, loving_drive_delta, Status.NeedsLove],
+	"trashy": [100.0, trashy_drive_delta, Status.Trashy]
 }
 
-export var sway_amount = 10.0
-export var shake_amount = 5.0
+var drive_threshold: float = 0.75
 
 var sway_direction_amount = [-sway_amount, sway_amount]
 var level: int = 0
-var status: int = Status.Content
 
 var sprite_frames_resources = [
 	"res://Resources/Pet0SpriteFrames.tres",
@@ -51,32 +70,89 @@ func _spawn_heart() -> void:
 	else:
 		get_parent().get_node("Audio").play_sfx("happy2")
 
+func _on_JumpTimer_timeout() -> void:
+	jump(250.0, true)
+	
+func _on_GrowTimer_timeout() -> void:
+	seconds_until_evolve -= 1
+	print("seconds_until_evolve %d" % seconds_until_evolve)
+	
+	if seconds_until_evolve <= 0:
+		grow(level + 1)
+		seconds_until_evolve = max_seconds_until_evolve
+	elif seconds_until_evolve == 2:
+		transition_into_black()
+	
 func _on_ui_feed_button_pressed() -> void:
 	_spawn_heart()
-	drive["hunger"][0] = min(drive["hunger"][0] + 5.0, 100.0)
+	drive["hunger"][0] = min(drive["hunger"][0] + food_reward_amount, 100.0)
+
+	if drive["hunger"][0] > drive_threshold and has_node("PetStatusIcon"):
+		get_node("PetStatusIcon").queue_free()
 	
 func _on_ui_drink_button_pressed() -> void:
 	_spawn_heart()
-	drive["thirst"][0] = min(drive["thirst"][0] + 5.0, 100.0)
+	drive["thirst"][0] = min(drive["thirst"][0] + drink_reward_amount, 100.0)
+	
+	if drive["thirst"][0] > drive_threshold and has_node("PetStatusIcon"):
+		get_node("PetStatusIcon").queue_free()
 	
 func _on_ui_play_button_pressed() -> void:
 	_spawn_heart()
-	drive["boring"][0] = min(drive["boring"][0] + 5.0, 100.0)
+	drive["boring"][0] = min(drive["boring"][0] + pet_reward_amount, 100.0)
+	if drive["boring"][0] > drive_threshold and has_node("PetStatusIcon"):
+		get_node("PetStatusIcon").queue_free()
 	
 func _on_ui_clean_button_pressed() -> void:
 	_spawn_heart()
-	drive["trashy"][0] = min(drive["trashy"][0] + 5.0, 100.0)
+	drive["trashy"][0] = min(drive["trashy"][0] + clean_reward_amount, 100.0)
+	if drive["trashy"][0] > drive_threshold and has_node("PetStatusIcon"):
+		get_node("PetStatusIcon").queue_free()
 	
 func _on_ui_pet_button_pressed() -> void:
 	_spawn_heart()
-	drive["loving"][0] = min(drive["loving"][0] + 5.0, 100.0)
-	
+	drive["loving"][0] = min(drive["loving"][0] + love_reward_amount, 100.0)
+	if drive["loving"][0] > drive_threshold and has_node("PetStatusIcon"):
+		get_node("PetStatusIcon").queue_free()
 
-func evolve(new_level: int) -> void:
+func transition_into_black() -> void:
+	var tween = Tween.new()
+	tween.interpolate_property(
+		$Sprite,
+		"modulate",
+		Color.white,
+		Color.black,
+		1.0,
+		Tween.TRANS_QUAD,
+		Tween.EASE_IN
+	)
+	add_child(tween)
+	tween.connect("tween_all_completed", self, "transition_into_white")
+	tween.start()
+
+func transition_into_white() -> void:
+	var tween = Tween.new()
+	tween.interpolate_property(
+		$Sprite,
+		"modulate",
+		Color.black,
+		Color.white,
+		2.0,
+		Tween.TRANS_QUAD,
+		Tween.EASE_IN,
+		1.0
+	)
+	add_child(tween)
+	tween.start()
+
+func grow(new_level: int) -> void:
 	level = new_level
 	var sprite_frames: SpriteFrames = load(sprite_frames_resources[new_level])
 	$Sprite.set_sprite_frames(sprite_frames)
-
+	
+	if level >= 2:
+		$GrowTimer.stop()
+		
 func sway() -> void:
 	$SwayTween.interpolate_property(
 		$Sprite,
@@ -251,66 +327,51 @@ func move(move_distance: float = 46.0) -> void:
 	)
 	$MoveTween.start()
 
-func _input(event):
-	# This event handler is just for testing using keys.
-	if event.is_pressed() and event is InputEventKey:
-		match event.scancode:
-			KEY_1:
-				jump(250.0, true)
-			KEY_2:
-				sway()
-				yield($SwayTween,"tween_all_completed")
-				sway_direction_amount.invert()
-				sway()
-				yield($SwayTween,"tween_all_completed")
-				stop_sway()
-			KEY_3:
-				shake()
-			KEY_4:
-				evolve((level+1)%3)
-			KEY_6:
-				set_status(Status.Content)
-			KEY_7:
-				set_status(Status.Bored)
-			KEY_8:
-				set_status(Status.Hungry)
-			KEY_9:
-				set_status(Status.NeedsAttention)
-			KEY_0:
-				set_status(Status.Sleepy)
-
 func remove_status_icons() -> void:
 	for child in get_children():
 		if child is PetStatusIcon:
 			child.queue_free()
 
-func set_status(new_status: int) -> void:
-	remove_status_icons()
-		
-	var pet_status_icon_scene = load("res://Interface/PetStatusIcon.tscn").instance()
+func show_icons() -> void:
+	var pet_status_icon_scene: AnimatedSprite = load("res://Interface/PetStatusIcon.tscn").instance()
 	
-	match new_status:
-		Status.Content:
-			$Sprite.set_animation("default")
-		Status.Bored:
-			pet_status_icon_scene.set_animation("bored")
-			$Sprite.set_animation("bored")
-		Status.Hungry:
-			pet_status_icon_scene.set_animation("hungry")
-			$Sprite.set_animation("hungry")
-		Status.NeedsAttention:
-			pet_status_icon_scene.set_animation("pet")
-		Status.Sleepy:
-			pet_status_icon_scene.set_animation("sleepy")
-			$Sprite.set_animation("sleepy")
+	var is_content: bool = true
+	for d in drive:
+		if drive[d][0]/100.0 <= drive_threshold:
+			var status: int = drive[d][2]
+			is_content = false
+			match status:
+				Status.Bored:
+					pet_status_icon_scene.set_animation("bored")
+					$Sprite.set_animation("bored")
+				Status.Hungry:
+					pet_status_icon_scene.set_animation("hungry")
+					$Sprite.set_animation("hungry")
+				Status.NeedsLove:
+					pet_status_icon_scene.set_animation("pet")
+				Status.Thirsty:
+					pet_status_icon_scene.set_animation("thirsty")
+					$Sprite.set_animation("hungry")
+				Status.Trashy:
+					pet_status_icon_scene.set_animation("trashy")
+					$Sprite.set_animation("hungry")
+				Status.Sleepy:
+					pet_status_icon_scene.set_animation("sleepy")
+					$Sprite.set_animation("sleepy")
+			
+			add_child(pet_status_icon_scene)
+			pet_status_icon_scene.set_position(Vector2(120.0, -100.0))
+			pet_status_icon_scene.float_up_and_down()
+			
+			break
 	
-	if new_status != Status.Content:
-		add_child(pet_status_icon_scene)
-		pet_status_icon_scene.set_position(Vector2(120.0, -100.0))
-		pet_status_icon_scene.float_up_and_down()
-				
-	status = new_status
+	if is_content:
+		$Sprite.set_animation("default")
 
 func _process(delta):
 	for d in drive:
 		drive[d][0] -= drive[d][1] * delta
+		
+		if not has_node("PetStatusIcon"):
+			show_icons()
+			
